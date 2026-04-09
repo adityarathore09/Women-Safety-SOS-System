90d742926fb0a003df06f52478293c8f76b4da15
adityarathore09 <72064472+adityarathore09@users.noreply.github.com>
2026-04-09 14:48:37 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -134,10 +134,6 @@ app.listen(PORT, "0.0.0.0", () => {
 });
     });
   }
-
-  app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
 }
 
 startServer();
